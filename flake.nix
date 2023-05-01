133997ae61da0aca8e9e725ce1d50f6f075d08bb
irishgreencitrus <irishgreencitrus@gmail.com>
2023-05-01 13:01:59 +0100

    Fix sourkraut

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "irishgreencitrus' NixOS configuration";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "nixpkgs/nixos-22.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
--- systems/sourkraut/configuration.nix
+++ systems/sourkraut/configuration.nix
@@ -3,6 +3,13 @@
     pkgs,
     ...
 }: {
+      imports = [ <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix> ];
+      virtualisation.vmVariant = {
+        virtualisation = {
+        memorySize = 4096; # Use 2048MiB memory.
+        cores = 4;         # Simulate 4 cores.
+        };
+      };
     users.users.lime = {
         isNormalUser = true;
     };
